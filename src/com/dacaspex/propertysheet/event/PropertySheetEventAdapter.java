6460e32a41743ae682fba5f3b2f430767a9120cb
Casper Smits <caspersmits11@outlook.com>
2018-09-03 00:34:46 +0200

    Added event adapter

---
+++ src/com/dacaspex/propertysheet/event/PropertySheetEventAdapter.java
@@ -0,0 +1,10 @@
+package com.dacaspex.propertysheet.event;
+
+import com.dacaspex.propertysheet.property.Property;
+
+public class PropertySheetEventAdapter implements PropertySheetEventListener {
+
+    @Override
+    public void onPropertyUpdated(Property property) {
+    }
+}
